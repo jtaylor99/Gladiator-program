729bf9d1336d07367966492c5664279a20867e0e
jtaylor99 <justicetaylor@basecampcodingacademy.org>
2018-07-09 16:21:09 -0500

    working on the attacking and testing the functions

---
+++ test_code.py
@@ -0,0 +1,29 @@
+from core import *
+
+
+def test_new_gladiator1():
+    result = new_gladiator(100, 0, 10, 20)
+    assert result == {
+        'health': 100,
+        'rage': 0,
+        'damage_low': 10,
+        'damage_high': 20
+    }
+
+
+def test_new_gladiator2():
+    result = new_gladiator(50, 30, 10, 20)
+    assert result == {
+        'health': 50,
+        'rage': 30,
+        'damage_low': 10,
+        'damage_high': 20
+    }
+
+
+def test_attack():
+    gladiator_1 = new_gladiator(100, 0, 20, 20)
+    gladiator_2 = new_gladiator(50, 30, 10, 20)
+    attack(gladiator_1, gladiator_2)
+    assert gladiator_1['rage'] == 15
+    assert gladiator_2['health'] == 30
