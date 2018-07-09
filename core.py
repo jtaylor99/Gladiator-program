8bfe4176c2e9e3d4c6ed96b5eb4730a255487fc6
jtaylor99 <justicetaylor@basecampcodingacademy.org>
2018-07-09 13:22:59 -0500

    starting on gladiator

---
+++ core.py
@@ -0,0 +1,6 @@
+def new_gladiators(health,rage,damage_low,damage_high):
+    health = 100
+    rage = 0
+    damage_low = 10
+    damage_high = 20
+    gladiators = {'health':100,'rage':0,'damage_low':10,'damage_high'}
