369d4d51173dd22c86a444244f08bc82113df33b
jtaylor99 <justicetaylor@basecampcodingacademy.org>
2018-07-10 11:02:11 -0500

    working on the shell

--- shell.py
+++ shell.py
@@ -1,5 +1,35 @@
-from core import *
+import core
 
 
 def get_name():
-    name = input('what\'s your name')
+    name_1 = input('What\'s your name?')
+    print('Welcome to the Game of Gladiators!', name.upper(), '!')
+    return name
+
+
+def get_name2():
+    name_2 = input('What\'s your name?')
+    print('Welcome to the Game of Gladiators!', name.upper(), '!')
+    return name
+
+
+def gladiator_1_turn():
+    print('{} turn'.format(get_name()))
+    while True:
+        action = input('What would you like to do?')
+        print('>>>>Attack')
+        print('>>>>Heal')
+        print('>>>>Pass')
+        print('>>>>Quit')
+    if action.lower == 'Attack':
+        core.attack(gladiator_2)
+    if action.lower == 'Heal':
+        core.heal(gladiator_1)
+    if action.lower == 'Pass':
+        break
+    if action.lower == 'Quit':
+        quit()
+    else:
+        print('Please choose a valid option!')
+
+def gladiator_2_turn():
