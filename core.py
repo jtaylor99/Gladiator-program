3dc2e0f85347782fd7e1083cc9e398488a275a8a
jtaylor99 <justicetaylor@basecampcodingacademy.org>
2018-07-10 09:03:38 -0500

    working on the healing

--- core.py
+++ core.py
@@ -1,6 +1,26 @@
-def new_gladiators(health,rage,damage_low,damage_high):
-    health = 100
-    rage = 0
-    damage_low = 10
-    damage_high = 20
-    gladiators = {'health':100,'rage':0,'damage_low':10,'damage_high'}
+from random import randint
+
+
+def new_gladiator(health, rage, damage_low, damage_high):
+    return {
+        'health': health,
+        'rage': rage,
+        'damage_low': damage_low,
+        'damage_high': damage_high
+    }
+
+
+def attack(attacker, defender):
+    crit_roll = randint(1, 100)
+    if crit_roll <= attacker['rage']:
+        crit_damage = damage * 2
+    else:
+        attacker['rage'] == 0
+    damage = randint(attacker['damage_low'], attacker['damage_high'])
+    defender['health'] = defender['health'] - damage
+    attacker['rage'] = attacker['rage'] + 15
+
+
+
+def 
+ 
