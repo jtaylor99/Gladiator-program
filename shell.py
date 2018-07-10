0843a8fb24eaa1942a12be53d876ef6cdd0acbb0
jtaylor99 <justicetaylor@basecampcodingacademy.org>
2018-07-10 16:12:03 -0500

    finishing the shell

--- shell.py
+++ shell.py
@@ -1,35 +1,53 @@
 import core
 
 
-def get_name():
+def get_name_and_assign_stats():
     name_1 = input('What\'s your name?')
-    print('Welcome to the Game of Gladiators!', name.upper(), '!')
-    return name
-
-
-def get_name2():
+    print('Welcome to the Game of Gladiators!', name_1.upper(), '!')
     name_2 = input('What\'s your name?')
-    print('Welcome to the Game of Gladiators!', name.upper(), '!')
-    return name
+    print('Welcome to the Game of Gladiators!', name_2.upper(), '!')
+    name_1 = core.new_gladiator(name_1, 100, 0, 10, 20)
+    name_2 = core.new_gladiator(name_2, 100, 0, 10, 20)
+    return name_1, name_2
 
 
-def gladiator_1_turn():
-    print('{} turn'.format(get_name()))
+def gladiator_turn(name_1, name_2):
+    print('{} turn'.format(name_1['Name'].upper()))
     while True:
-        action = input('What would you like to do?')
-        print('>>>>Attack')
-        print('>>>>Heal')
-        print('>>>>Pass')
-        print('>>>>Quit')
-    if action.lower == 'Attack':
-        core.attack(gladiator_2)
-    if action.lower == 'Heal':
-        core.heal(gladiator_1)
-    if action.lower == 'Pass':
-        break
-    if action.lower == 'Quit':
-        quit()
-    else:
-        print('Please choose a valid option!')
 
-def gladiator_2_turn():
+        print('>>>>[A]ttack')
+        print('>>>>[H]eal')
+        print('>>>>[P]ass')
+        print('>>>>[Q]uit')
+        action = input('{} What would you like to do?'.format(
+            name_1['Name'].upper()))
+        if action == 'A':
+            print(name_1['Name'], 'Attacks')
+            core.attack(name_1, name_2)
+            print('{} health is now at:{}'.format(name_2['Name'].upper(),
+                                                  name_2['health']))
+            return None
+        if action == 'H':
+            print(name_1['Name'], 'Heals')
+            core.heal(name_1)
+            print('{} health is now at:{}'.format(name_1['Name'].upper(),
+                                                  name_1['health']))
+
+            return None
+        if action == 'P':
+            break
+        if action == 'Q':
+            quit()
+        else:
+            print('Please choose a valid option!')
+
+
+def main():
+    name_1, name_2 = get_name_and_assign_stats()
+    while True:
+        gladiator_turn(name_1, name_2)
+        gladiator_turn(name_2, name_1)
+
+
+if __name__ == '__main__':
+    main()
