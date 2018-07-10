929bbf7a2659dc7dfd2c8640103eadd61667f8ae
jtaylor99 <justicetaylor@basecampcodingacademy.org>
2018-07-10 09:03:15 -0500

    working on the healing

--- test_code.py
+++ test_code.py
@@ -21,9 +21,17 @@ def test_new_gladiator2():
     }
 
 
-def test_attack():
+def test_attack1():
     gladiator_1 = new_gladiator(100, 0, 20, 20)
     gladiator_2 = new_gladiator(50, 30, 10, 20)
     attack(gladiator_1, gladiator_2)
     assert gladiator_1['rage'] == 15
     assert gladiator_2['health'] == 30
+
+
+def test_attack2():
+    gladiator_1 = new_gladiator(80, 15, 10, 20)
+    gladiator_2 = new_gladiator(100, 0, 10, 20)
+    attack(gladiator_2, gladiator_1)
+    assert gladiator_2['rage'] == 15
+    assert gladiator_1['health'] == 61
