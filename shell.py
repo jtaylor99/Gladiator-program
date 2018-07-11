af45b2609230f32961cdffe516fb12e49bb14586
jtaylor99 <justicetaylor@basecampcodingacademy.org>
2018-07-11 12:49:29 -0500

    I added an Ascii act to the program

--- shell.py
+++ shell.py
@@ -23,16 +23,25 @@ def gladiator_turn(name_1, name_2):
             name_1['Name'].upper()))
         if action == 'A':
             print(name_1['Name'].upper(), 'Attacks')
-            core.attack(name_1, name_2)
-            print('{} health is now at:{}'.format(name_2['Name'].upper(),
-                                                  name_2['health']))
-            return None
+            result = core.attack(name_1, name_2)
+            if result == 'miss':
+                print('{} misses'.format(name_1['Name'],
+                                         name_2['Name'].upper()))
+                return None
+            if result == 'crit':
+                print('critical damage!')
+                print('{} health is now at:{}'.format(name_2['Name'].upper(),
+                                                      name_2['health']))
+                return None
+            if result == 'hit':
+                print('{} health is now at:{}'.format(name_2['Name'].upper(),
+                                                      name_2['health']))
+                return None
         if action == 'H':
             print(name_1['Name'], 'Heals')
             core.heal(name_1)
             print('{} health is now at:{}'.format(name_1['Name'].upper(),
                                                   name_1['health']))
-
             return None
         if action == 'P':
             break
@@ -60,12 +69,50 @@ def is_dead(name_1, name_2):
 
 
 def main():
+    print('''           _..gggggppppp.._                       
+                  _.gd$$$$$$$$$$$$$$$$$$bp._                  
+               .g$$$$$$P^^""j$$b""""^^T$$$$$$p.               
+            .g$$$P^T$$b    d$P T;       ""^^T$$$p.            
+          .d$$P^"  :$; `  :$;                "^T$$b.          
+        .d$$P'      T$b.   T$b                  `T$$b.        
+       d$$P'      .gg$$$$bpd$$$p.d$bpp.           `T$$b       
+      d$$P      .d$$$$$$$$$$$$$$$$$$$$bp.           T$$b      
+     d$$P      d$$$$$$$$$$$$$$$$$$$$$$$$$b.          T$$b     
+    d$$P      d$$$$$$$$$$$$$$$$$$P^^T$$$$P            T$$b    
+   d$$P    '-'T$$$$$$$$$$$$$$$$$$bggpd$$$$b.           T$$b   
+  :$$$      .d$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$p._.g.     $$$;  
+  $$$;     d$$$$$$$$$$$$$$$$$$$$$$$P^"^T$$$$P^^T$$$;    :$$$  
+ :$$$     :$$$$$$$$$$$$$$:$$$$$$$$$_    "^T$bpd$$$$,     $$$; 
+ $$$;     :$$$$$$$$$$$$$$bT$$$$$P^^T$p.    `T$$$$$$;     :$$$ 
+:$$$      :$$$$$$$$$$$$$$P `^^^'    "^T$p.    lb`TP       $$$;
+:$$$      $$$$$$$$$$$$$$$              `T$$p._;$b         $$$;
+$$$;      $$$$$$$$$$$$$$;                `T$$$$:Tb        :$$$
+$$$;      $$$$$$$$$$$$$$$                        Tb    _  :$$$
+:$$$     d$$$$$$$$$$$$$$$.                        $b.__Tb $$$;
+:$$$  .g$$$$$$$$$$$$$$$$$$$p...______...gp._      :$`^^^' $$$;
+ $$$;  `^^'T$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$p.    Tb._, :$$$ 
+ :$$$       T$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$b.   "^"  $$$; 
+  $$$;       `$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$b      :$$$  
+  :$$$        $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$;     $$$;  
+   T$$b    _  :$$`$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$;   d$$P   
+    T$$b   T$g$$; :$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$  d$$P    
+     T$$b   `^^'  :$$ "^T$$$$$$$$$$$$$$$$$$$$$$$$$$$ d$$P     
+      T$$b        $P     T$$$$$$$$$$$$$$$$$$$$$$$$$;d$$P      
+       T$$b.      '       $$$$$$$$$$$$$$$$$$$$$$$$$$$$P       
+        `T$$$p.   bug    d$$$$$$$$$$$$$$$$$$$$$$$$$$P'        
+          `T$$$$p..__..g$$$$$$$$$$$$$$$$$$$$$$$$$$P'          
+            "^$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$^"            
+               "^T$$$$$$$$$$$$$$$$$$$$$$$$$$P^"               
+                   """^^^T$$$$$$$$$$P^^^"""
+''')
     name_1, name_2 = get_name_and_assign_stats()
     while True:
+        gladiator_turn(name_1, name_2)
         if is_dead(name_1, name_2):
             break
-        gladiator_turn(name_1, name_2)
         gladiator_turn(name_2, name_1)
+        if is_dead(name_1, name_2):
+            break
     is_winner(name_1, name_2)
 
 
